cd4e41737ce3dde07f6691b439addd2bac8705bf
axelkolo <gelyn.mandossi@gmail.com>
2023-01-15 18:34:21 +0100

    Defined the list of URLs to read from...

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import Parser from "rss-parser";
 const parser = new Parser();
 
 const main = async () => { //Define an async function to call
-    const url = "https://www.bonappetit.com/feed/recipes-rss-feed/rss";
+    const urls = ["https://www.bonappetit.com/feed/recipes-rss-feed/rss",
+                  "https://www.specialtyfood.com/rss/featured-articles/category/lunch/",
+                  "https://www.reddit.com/r/recipes/.rss"
+                ];
     const {title, items} = await parser.parseURL(url); //Fetch and parse the RSS feed XML and destructure the response to access the title and items
     console.log(title); //Print firld's title
     const results = items.map(({title, link}) => ({title, link})); //Extract only the title and link of each item and assign that new array to results
